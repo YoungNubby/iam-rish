5425061536d5aa65087aaf17bddc7267c95af5f6
Garrett Gilley <garrettgilley@Garretts-MacBook-Air.local>
2019-01-13 11:27:48 -0800

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Garrett Gilley on 1/13/19.
+//  Copyright © 2019 Garrett Gilley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
